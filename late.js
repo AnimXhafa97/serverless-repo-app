daaa6a1b9c8c7c0f96c19c592e6da23caaf5942b
AnimXhafa97 <animxhafa@gmail.com>
2022-06-12 23:22:55 -0400

    finished late.js issue #7

--- late.js
+++ late.js
@@ -1,1 +1,16 @@
 // start coding your function here!
+
+function running_late(date) {
+    const arg = new Date(date)
+    const latecheck = new Date(arg.getFullYear(), arg.getMonth(), arg.getDate(), '10', '00', '00', '000')
+
+    if (arg > latecheck) {
+        return 'It is late!'
+    } else {
+        return 'It is still early!'
+    }
+}
+running_late('December 17, 1995 9:59:59:999')
+
+
+exports.running_late = running_late;
